fae5bbafdb910a520e5edb0cb4eb78b0ce817bfd
rhinonet <zhao0233@163.com>
2021-09-01 12:02:51 +0800

    Update constellation.go
    add constellation index

--- constellation.go
+++ constellation.go
@@ -49,6 +49,45 @@ func (c Carbon) Constellation() string {
 	return ""
 }
 
+// Constellation gets constellation index
+// 获取星座索引
+func (c Carbon) ConstellationIndex() int {
+	index := -1
+
+	if c.IsInvalid() {
+		return index
+	}
+
+	switch {
+	case c.Month() == 3 && c.Day() >= 21, c.Month() == 4 && c.Day() <= 19:
+		index = 0 // 白羊座
+	case c.Month() == 4 && c.Day() >= 20, c.Month() == 5 && c.Day() <= 20:
+		index = 1 // 金牛座
+	case c.Month() == 5 && c.Day() >= 21, c.Month() == 6 && c.Day() <= 21:
+		index = 2 // 双子座
+	case c.Month() == 6 && c.Day() >= 22, c.Month() == 7 && c.Day() <= 22:
+		index = 3 // 巨蟹座
+	case c.Month() == 7 && c.Day() >= 23, c.Month() == 8 && c.Day() <= 22:
+		index = 4 // 狮子座
+	case c.Month() == 8 && c.Day() >= 23, c.Month() == 9 && c.Day() <= 22:
+		index = 5 // 处女座
+	case c.Month() == 9 && c.Day() >= 23, c.Month() == 10 && c.Day() <= 23:
+		index = 6 // 天秤座
+	case c.Month() == 10 && c.Day() >= 24, c.Month() == 11 && c.Day() <= 22:
+		index = 7 // 天蝎座
+	case c.Month() == 11 && c.Day() >= 23, c.Month() == 12 && c.Day() <= 21:
+		index = 8 // 射手座
+	case c.Month() == 12 && c.Day() >= 22, c.Month() == 1 && c.Day() <= 19:
+		index = 9 // 摩羯座
+	case c.Month() == 1 && c.Day() >= 20, c.Month() == 2 && c.Day() <= 18:
+		index = 10 // 水瓶座
+	case c.Month() == 2 && c.Day() >= 19, c.Month() == 3 && c.Day() <= 20:
+		index = 11 // 双鱼座
+	}
+	
+	return index
+}
+
 // IsAries whether is Aries.
 // 是否是白羊座
 func (c Carbon) IsAries() bool {
